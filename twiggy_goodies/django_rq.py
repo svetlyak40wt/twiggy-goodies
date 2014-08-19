6339ef0beb752aa4cbc4d1d8875c6d7e2f6e7c3d
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-08-19 21:33:38 +0000

    Django-rq's job decorator was fixed and now could be used with arguments, like the original one.

--- twiggy_goodies/django_rq.py
+++ twiggy_goodies/django_rq.py
@@ -12,10 +12,21 @@ def job(func_or_queue, connection=None, *args, **kwargs):
     it group all logged messages using uuid and sets
     job_name field as well."""
     decorated_func = _job(func_or_queue, connection=connection, *args, **kwargs)
-    
-    @wraps(decorated_func)
-    def wrapper(*args, **kwargs):
-        with log.fields(uuid=uuid.uuid4(),
-                        job_name=decorated_func.__name__):
-            return decorated_func(*args, **kwargs)
-    return wrapper
+
+    if callable(func_or_queue):
+        @wraps(decorated_func)
+        def wrapper(*args, **kwargs):
+            with log.fields(uuid=uuid.uuid4(),
+                            job_name=decorated_func.__name__):
+                return decorated_func(*args, **kwargs)
+        return wrapper
+    else:
+        def decorator(func):
+            @decorated_func
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                with log.fields(uuid=uuid.uuid4(),
+                                job_name=func.__name__):
+                    return func(*args, **kwargs)
+            return wrapper
+        return decorator
