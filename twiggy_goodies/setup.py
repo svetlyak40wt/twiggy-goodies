2fa7a6f5befabd11af2ea017808ce8a3c1f5f638
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-09-23 13:39:27 +0400

    Fixed issue when twiggy's plain text broke on unicode field values.

--- twiggy_goodies/setup.py
+++ twiggy_goodies/setup.py
@@ -8,11 +8,26 @@ from .json import JsonOutput
 from .std_logging import RedirectLoggingHandler
 
 
+_default_line_formatter = formats.line_format
+# twiggy tries to convert all values to string,
+# but when value is unicode this leads to UnicodeEncodeError
+# so it is better to make genericValue a smarter
+
+def smart_str(value):
+    if isinstance(value, unicode):
+        return value.encode('utf-8')
+    elif isinstance(value, str):
+        return value
+    return str(value)
+
+_default_line_formatter.conversion.genericValue = smart_str
+
+
 def setup_logging(filename,
                   level=levels.DEBUG,
                   format='json',
                   redirect=True,
-                  format_string=formats.line_format):
+                  format_string=_default_line_formatter):
 
     if filename is None:
         output = outputs.StreamOutput(format=format_string)
