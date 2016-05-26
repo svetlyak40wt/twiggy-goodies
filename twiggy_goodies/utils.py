3261de904d495e965e90a036d462cb0342d49054
Alexander Akhmetov <akhmetoff@gmail.com>
2016-05-26 18:05:07 +0300

    Ability to define your own format method in JsonOutput

--- twiggy_goodies/utils.py
+++ twiggy_goodies/utils.py
@@ -1,7 +1,20 @@
 import six
 
+from twiggy import levels
+
 
 def force_str(text, encoding='utf-8'):
     if six.text_type(text):
         return text
     return text.decode(encoding)
+
+
+def get_log_level_str(level):
+    severity_names = {
+        levels.CRITICAL: 'CRITICAL',
+        levels.DEBUG:    'DEBUG',
+        levels.ERROR:    'ERROR',
+        levels.INFO:     'INFO',
+        levels.WARNING:  'WARNING',
+    }
+    return severity_names[level]
